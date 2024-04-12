1ddc2b8779b7e4d543526c3f78e6111771445178
Varalakshmmi2793 <varaluckky.2@gmail.com>
2024-04-12 15:28:57 +0530

    error file updated

--- appview.js
+++ appview.js
@@ -1,3 +1,4 @@
+const path=require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -6,15 +7,21 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const contactussRoutes = require('./routes/contactus'); 
-const successRoutes = require('./routes/success'); 
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(contactussRoutes); 
-app.use(successRoutes); 
+app.use('/success', (req, res) => {
+    console.log('Success route accessed');
+    res.sendFile(path.join(__dirname,'views','success.html'));
+});
 
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname,'views','error.html'));
+});
 
 
 app.listen(3000, () => {
